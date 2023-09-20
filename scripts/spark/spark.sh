5872963b3e6e48d1d67bf2223dfe9f8bcfa1bcd0
xianglei <xianglei@exadoop.com>
2023-09-20 15:11:14 +0800

    add detect javahome

---
+++ scripts/spark/spark.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+BIGTOP_DEFAULTS_DIR=${BIGTOP_DEFAULTS_DIR-/etc/default}
+[ -n "${BIGTOP_DEFAULTS_DIR}" -a -r ${BIGTOP_DEFAULTS_DIR}/bigtop-utils ] && . ${BIGTOP_DEFAULTS_DIR}/bigtop-utils
+
+JAVA8_HOME_CANDIDATES=(
+    '/usr/java/jdk1.8'
+    '/usr/java/jre1.8'
+    '/usr/lib/jvm/j2sdk1.8-oracle'
+    '/usr/lib/jvm/j2sdk1.8-oracle/jre'
+    '/usr/lib/jvm/java-8-oracle'
+    '/usr/lib/jdk8-latest'
+    '/usr/lib/jvm/java-1.8.0'
+    '/usr/lib/jvm/java-1.8.0-oracle'
+)
+
+OPENJAVA8_HOME_CANDIDATES=(
+    '/usr/lib/jvm/java-1.8.0-openjdk-amd64'
+    '/usr/lib/jvm/java-1.8.0-openjdk-ppc64el'
+    '/usr/lib/jvm/java-1.8.0-openjdk'
+    '/usr/lib64/jvm/java-1.8.0-openjdk-1.8.0'
+    '/usr/lib/jvm/adoptopenjdk-8-hotspot'
+)
+
+MISCJAVA_HOME_CANDIDATES=(
+    '/Library/Java/Home'
+    '/usr/java/default'
+    '/usr/lib/jvm/java'
+    '/usr/lib/jvm/jre'
+    '/usr/lib/jvm/default-java'
+    '/usr/lib/jvm/java-openjdk'
+    '/usr/lib/jvm/jre-openjdk'
+    '/usr/lib/jvm/java-oracle'
+)
+
+case ${BIGTOP_JAVA_MAJOR} in
+  6|7) echo "Java ${BIGTOP_JAVA_MAJOR} is no longer supported. Please upgrade"
+     exit 1
+     ;;
+  8) JAVA_HOME_CANDIDATES=(${JAVA8_HOME_CANDIDATES[@]} ${OPENJAVA8_HOME_CANDIDATES[@]})
+     ;;
+  *) JAVA_HOME_CANDIDATES=(${JAVA8_HOME_CANDIDATES[@]}
+                           ${MISCJAVA_HOME_CANDIDATES[@]}
+                           ${OPENJAVA8_HOME_CANDIDATES[@]})
+     ;;
+esac
+
+# attempt to find java
+if [ -z "${JAVA_HOME}" ]; then
+  for candidate_regex in ${JAVA_HOME_CANDIDATES[@]} ; do
+      for candidate in `ls -rvd ${candidate_regex}* 2>/dev/null`; do
+        if [ -e ${candidate}/bin/java ]; then
+          export JAVA_HOME=${candidate}
+          break 2
+        fi
+      done
+  done
+fi
+
+function sparkOperation() {
+    
+}
