93a4aa4400bfc81037c69bd025fdcb96ab20c0d2
xianglei <xianglei@exadoop.com>
2024-03-22 13:49:59 +0800

    update log data

--- modules/grpc_server.py
+++ modules/grpc_server.py
@@ -245,7 +245,7 @@ class FileService(api_pb2_grpc.FileServiceServicer, Logger):
                 self._logger.debug("send: change file mode: {}".format(request.access_modes))
                 sudo.run_as_sudo('root', 'chmod ' + str(request.access_modes) + ' ' + tmp_filename)
 
-                self._logger.debug("send: change file owner to: {}, {}".format(request.owner, request.group))
+                self._logger.debug("send: change file owner to: {}, {}".format(final_user, final_group))
                 sudo.run_as_sudo('root', 'chown ' + final_user + ':' + final_group + ' ' + tmp_filename)
 
                 self._return_code = self.CODE[0]
@@ -275,7 +275,7 @@ class FileService(api_pb2_grpc.FileServiceServicer, Logger):
                 self._logger.debug("send: change file mode: {}".format(request.access_modes))
                 sudo.run_as_sudo('root', 'chmod ' + str(request.access_modes) + ' ' + tmp_filename)
 
-                self._logger.debug("send: change file owner to: {}, {}".format(request.owner, request.group))
+                self._logger.debug("send: change file owner to: {}, {}".format(final_user, final_group))
                 sudo.run_as_sudo('root', 'chown ' + final_user + ':' + final_group + ' ' + tmp_filename)
 
                 self._return_code = self.CODE[0]
