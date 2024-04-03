3744c62a61580fd9607ddb1608cc63fbebe115d4
xianglei <xianglei@exadoop.com>
2024-04-03 14:58:42 +0800

    add http test

--- unittests/http_test.py
+++ unittests/http_test.py
@@ -1,10 +1,50 @@
 import unittest
 import requests
+import json
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_sys_all(self):
+        params = {}
+        response = requests.get('http://localhost:7181/sys', data=json.dumps(params))
+        print(response.text)
+        self.assertEqual(response.status_code, 200)
+
+    def test_sys_cpu(self):
+        params = {}
+        response = requests.get('http://localhost:7181/sys/cpu', data=json.dumps(params))
+        print(response.text)
+        self.assertEqual(response.status_code, 200)
+
+    def test_sys_mem(self):
+        params = {}
+        response = requests.get('http://localhost:7181/sys/memory', data=json.dumps(params))
+        print(response.text)
+        self.assertEqual(response.status_code, 200)
+
+    def test_sys_storage(self):
+        params = {}
+        response = requests.get('http://localhost:7181/sys/storage', data=json.dumps(params))
+        print(response.text)
+        self.assertEqual(response.status_code, 200)
+
+    def test_sys_network(self):
+        params = {}
+        response = requests.get('http://localhost:7181/sys/network', data=json.dumps(params))
+        print(response.text)
+        self.assertEqual(response.status_code, 200)
+
+    def test_sys_services(self):
+        params = {"services": ["google"]}
+        response = requests.post('http://localhost:7181/sys/services', data=json.dumps(params))
+        print(response.text)
+        self.assertEqual(response.status_code, 200)
+
+    def test_sys_service(self):
+        params = {"service": "google"}
+        response = requests.post('http://localhost:7181/sys/service', data=json.dumps(params))
+        print(response.text)
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
