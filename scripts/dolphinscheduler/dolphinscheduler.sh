e53d73855b915c22cb09f36833ae1f1d842e6ff9
xianglei <xianglei@exadoop.com>
2024-04-09 16:19:47 +0800

    update dolphin script

--- scripts/dolphinscheduler/dolphinscheduler.sh
+++ scripts/dolphinscheduler/dolphinscheduler.sh
@@ -55,16 +55,76 @@ if [ -z "${JAVA_HOME}" ]; then
 fi
 
 function dolphinUsage() {
-    echo "Usage: $0 {start|stop|status|restart|operation}"
+    echo "Usage: $0 {master|worker|api|alert|standalone} {start|stop|status|restart|initDatabase}"
     exit 1
 }
 
 function dolphinOperation() {
-    if [ $1 = 'initDatabase' ];then
-        echo "initDatabase"
-        sudo ln -sf /usr/share/java/mysql-connector-j.jar /usr/lib/dolphinscheduler/lib/
-        sudo /usr/lib/dolphinscheduler/script/create-dolphinscheduler.sh
+  if [ $1 = 'master' ];then
+    if [ $2 = 'initDatabase' ]; then
+      echo "init database"
+      sudo ln -sf /usr/share/java/mysql-connector-j.jar /usr/lib/dolphinscheduler/lib/mysql-connector-java.jar
+      sudo /usr/lib/dolphinscheduler/script/create-dolphinscheduler.sh
+    elif [ $2 = 'start' ]; then
+      sudo systemctl start dolphinscheduler-master
+    elif [ $2 = 'stop' ]; then
+      sudo systemctl stop dolphinscheduler-master
+    elif [ $2 = 'status' ]; then
+      sudo systemctl status dolphinscheduler-master
+    elif [ $2 = 'restart' ]; then
+      sudo systemctl restart dolphinscheduler-master
     else
-        dolphinUsage
+      dolphinUsage
     fi
+  elif [ $1 = 'worker' ]; then
+    if [ $2 = 'start' ]; then
+      sudo systemctl start dolphinscheduler-worker
+    elif [ $2 = 'stop' ]; then
+      sudo systemctl stop dolphinscheduler-worker
+    elif [ $2 = 'status' ]; then
+      sudo systemctl status dolphinscheduler-worker
+    elif [ $2 = 'restart' ]; then
+      sudo systemctl restart dolphinscheduler-worker
+    else
+      dolphinUsage
+    fi
+  elif [ $1 = 'api' ]; then
+    if [ $2 = 'start' ]; then
+      sudo systemctl start dolphinscheduler-api
+    elif [ $2 = 'stop' ]; then
+      sudo systemctl stop dolphinscheduler-api
+    elif [ $2 = 'status' ]; then
+      sudo systemctl status dolphinscheduler-api
+    elif [ $2 = 'restart' ]; then
+      sudo systemctl restart dolphinscheduler-api
+    else
+      dolphinUsage
+    fi
+  elif [ $1 = 'alert' ]; then
+    if [ $2 = 'start' ]; then
+      sudo systemctl start dolphinscheduler-alert
+    elif [ $2 = 'stop' ]; then
+      sudo systemctl stop dolphinscheduler-alert
+    elif [ $2 = 'status' ]; then
+      sudo systemctl status dolphinscheduler-alert
+    elif [ $2 = 'restart' ]; then
+      sudo systemctl restart dolphinscheduler-alert
+    else
+      dolphinUsage
+    fi
+  elif [ $1 = 'standalone' ]; then
+    if [ $2 = 'start' ]; then
+      sudo systemctl start dolphinscheduler-standalone
+    elif [ $2 = 'stop' ]; then
+      sudo systemctl stop dolphinscheduler-standalone
+    elif [ $2 = 'status' ]; then
+      sudo systemctl status dolphinscheduler-standalone
+    elif [ $2 = 'restart' ]; then
+      sudo systemctl restart dolphinscheduler-standalone
+    else
+      dolphinUsage
+    fi
+  else
+    dolphinUsage
+  fi
 }
