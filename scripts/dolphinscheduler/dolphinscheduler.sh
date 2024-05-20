e0ca3b948795578b174d980eb1e5a524c06d015a
xianglei <xianglei@exadoop.com>
2024-05-20 15:55:33 +0800

    update dolphin script

--- scripts/dolphinscheduler/dolphinscheduler.sh
+++ scripts/dolphinscheduler/dolphinscheduler.sh
@@ -60,12 +60,35 @@ function dolphinUsage() {
 }
 
 function dolphinOperation() {
+  CONNECTOR_JAR_STATUS=$(rpm -qa | grep mysql-connector-j; echo $?)
   if [ $1 = 'master' ];then
     if [ $2 = 'initDatabase' ]; then
+      MYSQL_USER=$3
+      MYSQL_PASSWORD=$4
+      MYSQL_DB=dolphinscheduler
       echo "init database"
-      sudo ln -sf /usr/share/java/mysql-connector-j.jar /usr/lib/dolphinscheduler/lib/mysql-connector-java.jar
-      sudo /usr/lib/dolphinscheduler/script/create-dolphinscheduler.sh
+      echo "Install mysql client and jdbc driver"
+      if [ $CONNECTOR_JAR_STATUS -ne 0 ]; then
+        sudo yum -y install mysql-connector-j mysql --verbose
+      fi
+      echo "Link mysql jdbc driver to /usr/lib/dolphinscheduler/lib/mysql-connector-java.jar"
+      if [ -L /usr/lib/dolphinscheduler/master-server/libs/mysql-connector-java.jar ]; then
+        sudo rm -f /usr/lib/dolphinscheduler/master-server/libs/mysql-connector-java.jar
+      fi
+      sudo ln -sf /usr/share/java/mysql-connector-j.jar /usr/lib/dolphinscheduler/master-server/libs/mysql-connector-java.jar
+      echo "Create database"
+      sudo mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB"
+      echo "Create user and grant privileges"
+      sudo mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE USER IF NOT EXISTS 'dolphinscheduler'@'%' IDENTIFIED BY 'dolphinscheduler';"
+      sudo mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO 'dolphinscheduler'@'%';"
+      sudo mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "FLUSH PRIVILEGES;"
+      sudo mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DB < /usr/lib/dolphinscheduler/tools/sql/sql/dolphinscheduler_mysql.sql
     elif [ $2 = 'start' ]; then
+      sudo mkdir -p /dolphinscheduler
+      OWNER=$(stat -c %U:%G /dolphinscheduler)
+      if [ $OWNER != "dolphinscheduler:dolphinscheduler" ]; then
+        chown -R dolphinscheduler:dolphinscheduler /dolphinscheduler
+      fi
       sudo systemctl start dolphinscheduler-master
     elif [ $2 = 'stop' ]; then
       sudo systemctl stop dolphinscheduler-master
@@ -78,7 +101,19 @@ function dolphinOperation() {
     fi
   elif [ $1 = 'worker' ]; then
     if [ $2 = 'start' ]; then
+      sudo mkdir -p /dolphinscheduler
+      if [ $OWNER != "dolphinscheduler:dolphinscheduler" ]; then
+        chown -R dolphinscheduler:dolphinscheduler /dolphinscheduler
+      fi
       sudo systemctl start dolphinscheduler-worker
+    elif [ $2 = "jdbc" ]; then
+      if [ $CONNECTOR_JAR_STATUS -ne 0 ]; then
+        sudo yum -y install mysql-connector-j mysql --verbose
+      fi
+      if [ -L /usr/lib/dolphinscheduler/worker-server/libs/mysql-connector-java.jar ]; then
+        sudo rm -f /usr/lib/dolphinscheduler/worker-server/libs/mysql-connector-java.jar
+      fi
+      sudo ln -sf /usr/share/java/mysql-connector-java.jar /usr/lib/dolphinscheduler/worker-server/libs/mysql-connector-java.jar
     elif [ $2 = 'stop' ]; then
       sudo systemctl stop dolphinscheduler-worker
     elif [ $2 = 'status' ]; then
@@ -90,7 +125,19 @@ function dolphinOperation() {
     fi
   elif [ $1 = 'api' ]; then
     if [ $2 = 'start' ]; then
+      sudo mkdir -p /dolphinscheduler
+      if [ $OWNER != "dolphinscheduler:dolphinscheduler" ]; then
+        chown -R dolphinscheduler:dolphinscheduler /dolphinscheduler
+      fi
       sudo systemctl start dolphinscheduler-api
+    elif [ $2 = "jdbc" ]; then
+      if [ $CONNECTOR_JAR_STATUS -ne 0 ]; then
+        sudo yum -y install mysql-connector-j mysql --verbose
+      fi
+      if [ -L /usr/lib/dolphinscheduler/api-server/libs/mysql-connector-java.jar ]; then
+        sudo rm -f /usr/lib/dolphinscheduler/api-server/libs/mysql-connector-java.jar
+      fi
+      sudo ln -sf /usr/share/java/mysql-connector-java.jar /usr/lib/dolphinscheduler/api-server/libs/mysql-connector-java.jar
     elif [ $2 = 'stop' ]; then
       sudo systemctl stop dolphinscheduler-api
     elif [ $2 = 'status' ]; then
@@ -102,7 +149,19 @@ function dolphinOperation() {
     fi
   elif [ $1 = 'alert' ]; then
     if [ $2 = 'start' ]; then
+      mkdir -p /dolphinscheduler
+      if [ $OWNER != "dolphinscheduler:dolphinscheduler" ]; then
+        chown -R dolphinscheduler:dolphinscheduler /dolphinscheduler
+      fi
       sudo systemctl start dolphinscheduler-alert
+    elif [ $2 = "jdbc" ]; then
+      if [ $CONNECTOR_JAR_STATUS -ne 0 ]; then
+        sudo yum -y install mysql-connector-j mysql --verbose
+      fi
+      if [ -L /usr/lib/dolphinscheduler/alert-server/libs/mysql-connector-java.jar ]; then
+        sudo rm -f /usr/lib/dolphinscheduler/alert-server/libs/mysql-connector-java.jar
+      fi
+      sudo ln -sf /usr/share/java/mysql-connector-java.jar /usr/lib/dolphinscheduler/alert-server/libs/mysql-connector-java.jar
     elif [ $2 = 'stop' ]; then
       sudo systemctl stop dolphinscheduler-alert
     elif [ $2 = 'status' ]; then
@@ -114,7 +173,19 @@ function dolphinOperation() {
     fi
   elif [ $1 = 'standalone' ]; then
     if [ $2 = 'start' ]; then
+      sudo mkdir -p /dolphinscheduler
+      if [ $OWNER != "dolphinscheduler:dolphinscheduler" ]; then
+        chown -R dolphinscheduler:dolphinscheduler /dolphinscheduler
+      fi
       sudo systemctl start dolphinscheduler-standalone
+    elif [ $2 = "jdbc" ]; then
+      if [ $CONNECTOR_JAR_STATUS -ne 0 ]; then
+        sudo yum -y install mysql-connector-j mysql --verbose
+      fi
+      if [ -L /usr/lib/dolphinscheduler/standalone-server/libs/mysql-connector-java.jar ]; then
+        sudo rm -f /usr/lib/dolphinscheduler/standalone-server/libs/mysql-connector-java.jar
+      fi
+      sudo ln -sf /usr/share/java/mysql-connector-java.jar /usr/lib/dolphinscheduler/standalone-server/libs/mysql-connector-java.jar
     elif [ $2 = 'stop' ]; then
       sudo systemctl stop dolphinscheduler-standalone
     elif [ $2 = 'status' ]; then
