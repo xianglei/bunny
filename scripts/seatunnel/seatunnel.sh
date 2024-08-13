866354de286a01981b2da7306ec8d054c3ca3bd5
xianglei <xianglei@exadoop.com>
2024-08-13 16:11:08 +0800

    add seatunnel

---
+++ scripts/seatunnel/seatunnel.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+
+JAVA8_HOME_CANDIDATES=(
+    '/usr/java/jdk1.8'
+    '/usr/java/jre1.8'
+    '/usr/lib/jvm/j2sdk1.8-oracle'
+    '/usr/lib/jvm/j2sdk1.8-oracle/jre'
+    '/usr/lib/jvm/java-8-oracle'
+    '/usr/lib/jdk8-latest'
+    '/usr/lib/jvm/java-1.8.0'
+    '/usr/lib/jvm/java-1.8.0-oracle'
+    '/usr/lib/jvm/java-1.8.0-oracle'
+    '/usr/lib/jvm/jdk-1.8-oracle-aarch64'
+    '/usr/lib/jvm/jdk-1.8-oracle-x64'
+    '/usr/lib/jvm/jdk-1.8-oracle-x86'
+    '/usr/lib/jvm/jdk-1.8-oracle'
+    '/usr/java/latest'
+)
+
+OPENJAVA8_HOME_CANDIDATES=(
+    '/usr/lib/jvm/java-1.8.0-openjdk-amd64'
+    '/usr/lib/jvm/java-1.8.0-openjdk-ppc64el'
+    '/usr/lib/jvm/java-1.8.0-openjdk'
+    '/usr/lib64/jvm/java-1.8.0-openjdk-1.8.0'
+    '/usr/lib/jvm/adoptopenjdk-8-hotspot'
+)
+
+MISCJAVA_HOME_CANDIDATES=(
+    '/Library/Java/Home'
+    '/usr/java/default'
+    '/usr/lib/jvm/java'
+    '/usr/lib/jvm/jre'
+    '/usr/lib/jvm/default-java'
+    '/usr/lib/jvm/java-openjdk'
+    '/usr/lib/jvm/jre-openjdk'
+    '/usr/lib/jvm/java-oracle'
+)
+
+case ${BIGTOP_JAVA_MAJOR} in
+  6|7) echo "Java ${BIGTOP_JAVA_MAJOR} is no longer supported. Please upgrade"
+     exit 1
+     ;;
+  8) JAVA_HOME_CANDIDATES=(${JAVA8_HOME_CANDIDATES[@]} ${OPENJAVA8_HOME_CANDIDATES[@]})
+     ;;
+  *) JAVA_HOME_CANDIDATES=(${JAVA8_HOME_CANDIDATES[@]}
+                           ${MISCJAVA_HOME_CANDIDATES[@]}
+                           ${OPENJAVA8_HOME_CANDIDATES[@]})
+     ;;
+esac
+
+# attempt to find java
+if [ -z "${JAVA_HOME}" ]; then
+  for candidate_regex in ${JAVA_HOME_CANDIDATES[@]} ; do
+      for candidate in `ls -rvd ${candidate_regex}* 2>/dev/null`; do
+        if [ -e ${candidate}/bin/java ]; then
+          export JAVA_HOME=${candidate}
+          break 2
+        fi
+      done
+  done
+fi
+
+HOSTNAME=$(hostname -f)
+
+function seatunnelOperation() {
+  if [ -z "${JAVA_HOME}" ]; then
+    echo "JAVA_HOME is not set. Please set JAVA_HOME."
+    exit 1
+  fi
+  if [ $1 = "start" ]; then
+    systemctl start seatunnel
+  elif [ $1 = "stop" ]; then
+    systemctl stop seatunnel
+  elif [ $1 = "status" ]; then
+    systemctl status seatunnel
+  elif [ $1 = "restart" ]; then
+    systemctl restart seatunnel
+  elif [ $1 = "enable" ]; then
+    systemctl enable seatunnel
+  elif [ $1 = "disable" ]; then
+    systemctl disable seatunnel
+  elif [ $1 = "mkdir" ]; then
+    mkdir -p $2
+    chown -R seatunnel:seatunnel $2
+  elif [ $1 = "mkhdfsdir" ]; then
+    sudo -u hdfs hdfs dfs -mkdir -p $2
+    sudo -u hdfs hdfs dfs -chown seatunnel:seatunnel $2
+  else
+    echo "Invalid operation. Please use start, stop or status."
+    exit 1
+  fi
+}
